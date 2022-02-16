4ef73b8d78f0ec0e034961f15931100d5fd88d73
Aindulgence <98210808+Aindulgence@users.noreply.github.com>
2022-02-16 23:03:47 +0800

    Add files via upload

---
+++ Project7/text.c
@@ -0,0 +1,130 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+//判断素数：(试除法) 100到200之间的素数
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2; j <= sqrt(i); j++) //sqrt(i)表示i开平方，是数学库函数
+		{
+			if(i%j==0)
+			{
+				break;
+			}
+		}
+	    if (j > sqrt(i))
+		{
+			printf("%d ", i);
+			count++;
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}
+
+////1000到2000之间的闰年
+//int main()
+//{
+//	int year = 0;
+//	int count = 0;
+//	for (year = 1000; year <= 2000; year++)
+//	{
+//		if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+//		{
+//			printf("%d ", year);
+//			count++;
+//		}
+//	}
+//	printf("\ncount=%d\n", count);
+//	return 0;
+//}
+
+////辗转相除法求两个数的最大公约数
+//int main()
+//{
+//	int m = 24;
+//	int n = 18;
+//	int r = 0;
+//	scanf("%d%d", &m, &n);
+//	while (r = m % n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 1;
+//	int j = 1;
+//	while (i <= 4)
+//	{
+//		j = j * i;
+//		i++;
+//	}
+//	printf("%d\n", j);
+//	return 0;
+//}
+
+//void text()
+//{
+//	static int a = 1;
+//    //static静态的；静电的
+//	a++;
+//	printf("a=%d\n", a);
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 5)
+//	{
+//		text();
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//	*pc = 'a';
+//	printf("%c\n", ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	*p = 20;
+//	printf("a=%d\n", a);
+//	//printf("a=%p\n", a);
+//	//printf("%p\n", p);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int input = 0;
+//	printf("加入比特\n");
+//	printf("你要好好学习吗？（1/0）>:");
+//	scanf_s("%d,&input");
+//	if (input == 1)
+//		printf("好offer\n");
+//	else
+//		printf("卖红薯\n");
+//
+//	return 0;
+//}
